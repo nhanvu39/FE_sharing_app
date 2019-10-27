85181abcd19e7b4f3877e310fc9acddafb0cdbbe
VanSon999 <nguyenvanson95999@gmail.com>
2019-10-27 12:00:47 +0700

    gg

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
     include 'layout_head.php';
 ?>
-<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
+<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/contact.jpg');">
 		<h2 class="ltext-105 cl0 txt-center">
 			Contact With Us
 		</h2>
