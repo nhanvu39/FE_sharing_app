0b088b6efc08c9872cd882cfcfb96423dc4d869f
nhanvu39 <nhanvu1998@gmail.com>
2019-12-09 22:38:50 +0700

    fixDB

--- database/ass.sql
+++ database/ass.sql
@@ -2,10 +2,10 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 09, 2019 at 04:18 PM
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2019 at 04:38 PM
 -- Server version: 10.4.6-MariaDB
--- PHP Version: 7.1.32
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -87,16 +87,18 @@ CREATE TABLE `users` (
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `loc` tinyint(1) NOT NULL
+  `loc` tinyint(1) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `userName`, `password`, `firstName`, `lastName`, `email`, `loc`) VALUES
-(1, 'lasdtiger', '123456', 'value-4', 'value-5', '', 0),
-(2, 'nhanvu', '073deebe90916d67db7a2e3ef96c0989', 'VÅ©', 'Tráº§n', 'nhanvu1998@gmail.com', 0);
+INSERT INTO `users` (`id`, `userName`, `password`, `firstName`, `lastName`, `email`, `loc`, `isAdmin`) VALUES
+(1, 'lasdtiger', '123456', 'value-4', 'value-5', '', 0, 1),
+(2, 'nhanvu', '073deebe90916d67db7a2e3ef96c0989', 'VÅ©', 'Tráº§n', 'nhanvu1998@gmail.com', 0, 0),
+(3, 'asdfgh', '202cb962ac59075b964b07152d234b70', 'wertwetetertertertreter', 'nhan', 'sao_nho_ngoi@yahoo.com', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -142,7 +144,7 @@ ALTER TABLE `software`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
